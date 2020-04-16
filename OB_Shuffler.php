e8dd6c5bf3f0d7a74e475c5c2a69071a7555e3e6
Johnny Garcia <johnnyg2015@gmail.com>
2020-04-16 17:01:44 -0600

    clean up code

--- OB_Shuffler.php
+++ OB_Shuffler.php
@@ -3,26 +3,28 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Onboarding Group Generator</title>
   </head>
   <body>
     <center><h1>Onboarding Small Group Generator</h1></center>
+    <!--Create new shuffler object, group size of 3-->
     <?php $shuffle1 = new Shuffler(3) ?>
     <style>
       h1 {
         margin-bottom: 40px;
       }
+
       .group {
         margin-left: 10px;
         margin-right: 10px;
         flex-basis: 13%;
       }
+
       #groups_body {
         display: flex;
         flex-wrap: wrap;
         justify-content: center;
         margin-left: 5% !important;
-
       }
 
       .group_heading{
@@ -31,19 +33,11 @@
 
       h3 {
         margin-bottom: 0px;
-
       }
-
-      li {
-
-      }
-
     </style>
 
     <?php
     class Shuffler {
-
-
       public static $ob_leaders = array("Chris", "Bo", "Josh", "Josue", "Eric", "Neubs", "Brittany");
       public static $ob_members = array("Alaine", "Alex Monroe", "David A", "Davis", "Enzo", "Halle", "Kevin", "Matthew W",
       "Sam", "Sean M", "Austin", "Abby", "Antonio", "Clare", "JT", "Lauren", "Tori", "Matt Francis", "James McCoy",
@@ -52,7 +46,6 @@
       public $groupsArray = array();
       public $count = 1;
       public $membersPerGroup;
-      public $group1 = array();
 
       public function __construct($membersPerGroup){
 
@@ -62,8 +55,6 @@
         echo "<div id='groups_body'>";
         $this->printGroups();
         echo "</div>";
-
-
       }
 
     //shuffles initial groups
@@ -95,11 +86,7 @@
           $this->groupsArray[6][] = $this->groupsArray[7][0];
           array_splice($this->groupsArray, 7, 8);
         }
-
       }
-
-
-
     }
 
     ?>
